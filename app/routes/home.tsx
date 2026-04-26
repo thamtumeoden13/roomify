e27f0af11d066aa74311a869b081975145933744
BlackCat13 <detective.blackcat13@gmail.com>
2026-04-26 13:21:03 +0700

    host and upload images

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -5,6 +5,8 @@ import Button from "../../components/ui/Button";
 import Upload from "../../components/Upload";
 import {useNavigate} from "react-router";
 import {MAX_UPLOAD_BYTES} from "../../lib/constants";
+import {useState} from "react";
+import {createProject} from "../../lib/puter.action";
 
 export function meta({}: Route.MetaArgs) {
     return [
@@ -16,10 +18,36 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
 
     const navigate = useNavigate();
+    const [projects, setProjects] = useState<DesignItem[]>([]);
 
-    const handleUploadComplete = (base64Image: string) => {
+    const handleUploadComplete = async (base64Image: string) => {
         const newId = Date.now().toString();
-        navigate(`/visualizer/${newId}`, {state: {image: base64Image}});
+        const name = `Residence ${newId}`;
+
+        const newItem = {
+            id: newId, name, sourceImage: base64Image,
+            renderedImage: undefined,
+            timestamp: Date.now(),
+        }
+
+        const saved = await createProject({item: newItem, visibility: 'public'});
+
+        if (!saved) {
+            console.error("Failed to save project");
+            return false;
+        }
+
+        setProjects((prev) => [newItem, ...prev]);
+
+        navigate(`/visualizer/${newId}`, {
+            state: {
+                initialImage: saved.sourceImage,
+                initialRendered: saved.renderedImage || null,
+                name: saved.name
+            }
+        });
+
+        return true;
     }
 
     return (
@@ -84,31 +112,34 @@ export default function Home() {
                         </div>
                     </div>
                     <div className={"projects-grid"}>
-                        <div className={"project-card group"}>
-                            <div className="preview">
-                                <img
-                                    src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png"
-                                    alt="project preview"/>
+                        {projects.map(({id, name, renderedImage, sourceImage, timestamp}: DesignItem, i: number) => (
+                            <div className={"project-card group"}>
+                                <div className="preview">
+                                    <img
+                                        src={renderedImage || sourceImage}
+                                        alt="project preview"
+                                    />
 
-                                <div id="badge">
-                                    <span>Community</span>
-                                </div>
-                            </div>
-
-                            <div className="card-body">
-                                <div>
-                                    <h3>Project Manhattan</h3>
-                                    <div className="meta">
-                                        <Clock size={12}/>
-                                        <span>{new Date('01.01.2027').toLocaleDateString()}</span>
-                                        <span>By BlackCat 13</span>
+                                    <div id="badge">
+                                        <span>Community</span>
                                     </div>
                                 </div>
-                                <div className={"arrow"}>
-                                    <ArrowUpRight size={18}/>
+
+                                <div className="card-body">
+                                    <div>
+                                        <h3>{name}</h3>
+                                        <div className="meta">
+                                            <Clock size={12}/>
+                                            <span>{new Date(timestamp).toLocaleDateString()}</span>
+                                            <span>By BlackCat 13</span>
+                                        </div>
+                                    </div>
+                                    <div className={"arrow"}>
+                                        <ArrowUpRight size={18}/>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        ))}
                     </div>
                 </div>
             </section>
